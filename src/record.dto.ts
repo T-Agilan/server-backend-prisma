251a96e07cfab473780b80810cd3fb0aafc79550
T-Agilan <agilanakil1432@gmail.com>
2023-12-29 11:04:15 +0530

    added remain input field

--- src/record.dto.ts
+++ src/record.dto.ts
@@ -1,8 +1,34 @@
 // server/src/modules/graphql/dto/record.dto.ts
-import {  Field, InputType } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
-export class RecordTypeDto  {
-  @Field() 
-  name: string;
+export class RecordTypeDto {
+  @Field()
+  fname: string;
+  @Field()
+  mail: string;
+  @Field()
+  number: string;
+  @Field()
+  website: string;
+  @Field()
+  contactName: string;
+  @Field()
+  contactPhone: string;
+  @Field()
+  contactMail: string;
+  @Field()
+  notes: string;
+  @Field()
+  type: string;
+  @Field()
+  category: string;
+  @Field()
+  percentage: number;
+  @Field()
+  activeFrom: string;
+  @Field()
+  criticalAccount: boolean;
+  @Field()
+  paymentOptions: string;
 }
